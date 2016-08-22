a982951bd9b6d6512d7b391db11f0585a4e6ebb4
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-08-22 19:30:26 +1000

    api.ai integration

--- index.js
+++ index.js
@@ -56,6 +56,10 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
     process.exit(1);
 }
 
+var apiai = require('botkit-middleware-apiai')({
+   token: process.env.APIAI_TOKEN
+});
+controller.middleware.receive.use(apiai.receive);
 
 /**
  * A demonstration for how to handle websocket events. In this case, just log when we have and have not
@@ -85,10 +89,27 @@ controller.on('bot_channel_join', function (bot, message) {
     bot.reply(message, "I'm here!")
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
+controller.hears(['hello'], ['mention', 'direct_mention', 'direct_message'], apiai.hears, function (bot, message) {
     bot.reply(message, 'Hello!');
+    bot.api.reactions.add({
+        timestamp:message.ts,
+        channel:message.channel,
+        name: 'iamyourmaster'
+    },function(err){
+        if(err){
+            console.log(err);
+        }
+        bot.reply(message, 'Reacting to some stuff...');
+    })
 });
 
+controller.hears(['flights'], ['mention', 'direct_mention', 'direct_message'], apiai.hears, function (bot, message) {
+   if(message.fulfillment.speech !== '') {
+       bot.reply(message, message.fulfillment.speech);
+   } else {
+       bot.reply(message, "You requested to fly to " + message.entities['geo-city'] + " on " + message.entities['date']+".");
+   }
+});
 
 /**
  * AN example of what could be:
